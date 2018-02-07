abeb2486c8dfc7e322a13138188b78f6f9d08c33
Sean Glennon <seanglennon2@gmail.com>
2018-02-06 21:33:17 -0600

    Initialize Repo

---
+++ Gemfile
@@ -0,0 +1,32 @@
+source 'https://rubygems.org'
+ruby '2.0.0'
+
+
+gem 'rails', '4.0.8'
+
+gem 'pg', '~> 0.20'
+
+gem 'sass-rails', '4.0.3'
+
+gem 'simple_form', '~> 3.1.0'
+
+gem 'bootstrap-sass', '~> 3.3.4.1'
+
+gem 'devise', '~> 3.4.1'
+
+gem 'uglifier', '2.1.1'
+
+gem 'coffee-rails', '4.0.1'
+
+gem 'jquery-rails', '3.0.4'
+
+gem 'turbolinks', '1.1.1'
+
+gem 'jbuilder', '1.0.2'
+
+gem 'therubyracer'
+
+group :doc do
+  gem 'sdoc', require: false
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MessageBoard::Application.config.secret_key_base = 'c06d2f142ac77152ca75d048157bf87506c1bddebf8f0e072ca974ff9258909f9fc6db12fab40252fffcda2a8d42bd8280d810cb1c778e019b7b5c0f7f614b1b'
